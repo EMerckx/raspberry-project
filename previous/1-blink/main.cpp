258bd00c4293c9a51c7f24e0d97c1add2e6fc198
EMerckx <ewoutmerckx.pro@gmail.com>
2015-09-06 15:03:12 +0200

    formatted code

--- previous/1-blink/main.cpp
+++ previous/1-blink/main.cpp
@@ -4,17 +4,17 @@
 using std::cout;
 using std::endl;
 
-int main(){
-  wiringPiSetup();
-  pinMode(0,OUTPUT);
-  for(int i=0; i<100; i++){
-    digitalWrite(0, HIGH);
-    cout << "LED on" << endl;
-    delay(500);
-    digitalWrite(0, LOW);
-    cout << "LED off" << endl;
-    delay(500);
-  }
+int main() {
+    wiringPiSetup();
+    pinMode(0, OUTPUT);
+    for (int i = 0; i < 100; i++) {
+        digitalWrite(0, HIGH);
+        cout << "LED on" << endl;
+        delay(500);
+        digitalWrite(0, LOW);
+        cout << "LED off" << endl;
+        delay(500);
+    }
 
-  return 0;
+    return 0;
 }
