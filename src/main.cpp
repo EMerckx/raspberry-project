7e82b74174d17cf691fbf6403aec477ee1d2c797
Ewout Merckx <ewoutmerckx.pro@gmail.com>
2017-05-26 14:35:45 +0200

    updated main using port and to string

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+#include "port.h"
+
 using std::cout;
 using std::endl;
 
 int main() {
     cout << "Hello World!" << endl; 
+    
+    cout << "Creating output port." << endl; 
+
+    Port<OUTPUT> p(25, 1);
+
+    cout << "Printing output port." << endl;
+
+    cout << p.to_string() << endl;    
+
     return 0;
 
 }
