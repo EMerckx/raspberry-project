75e2b0706ff1f55e6085d68eb0e2ba4aa211469d
Ewout Merckx <ewoutmerckx.pro@gmail.com>
2017-06-03 23:29:16 +0200

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,30 @@
 using std::cout;
 using std::endl;
 
+// pin name = pin number
+const int ENA = 5;
+const int ENB = 6;
+const int IN1 = 0;
+const int IN2 = 1;
+const int IN3 = 2;
+const int IN4 = 3;
+
+Port<OUTPUT> ena;
+Port<OUTPUT> enb;
+
+
+void init() {
+  // pins ENA and ENB to state high
+  ena = Port<OUTPUT>(ENA, HIGH);
+  enb = Port<OUTPUT>(ENB, HIGH);
+}
+
+void stop() {
+  // pins ENA and ENB to state high
+  ena.state(LOW);
+  enb.state(LOW);
+}
+
 int main() {
     cout << "Hello World!" << endl; 
     
@@ -23,6 +47,18 @@ int main() {
     cout << "Printing output port." << endl;
 
     cout << p << endl;
+
+    // second half
+
+    init();
+
+    cout << ena << endl << enb << endl;
+
+    delay(3000);
+
+    stop();
+
+    cout << ena << endl << enb << endl;
 
     return 0;
 
