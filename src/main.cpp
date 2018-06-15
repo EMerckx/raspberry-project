badb184b3bc95a587cdec324a46f0972ad4f61c6
EMerckx <ewoutmerckx.pro@gmail.com>
2018-06-15 21:49:55 +0000

    added prefix with time in logging statements

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <ctime>
 #include <iostream>
+#include <string>
 #include <wiringPi.h>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 // amount of steps is a product of 4
 const int STEPS = 4;
@@ -13,6 +16,21 @@ const int IN1 = 0;
 const int IN2 = 1;
 const int IN3 = 2;
 const int IN4 = 3;
+
+//********************************************************************************************
+
+string getCurrentTime() {
+  std::time_t result = std::time(nullptr);
+  string str = std::asctime(std::localtime(&result));
+  return str.substr(0, str.length() - 1);
+}
+
+string getLogPrefix() {
+  string str;
+  str.append(getCurrentTime());
+  str.append(" - ");
+  return str;
+}
 
 //********************************************************************************************
 
@@ -64,22 +82,22 @@ void rightWheelsBackward() {
 //********************************************************************************************
 
 void goForward() {
-  cout << "Going forward." << endl;
+  cout << getLogPrefix() << "Going forward." << endl;
   setStep(HIGH, LOW, HIGH, LOW);
 }
 
 void goBackward() {
-  cout << "Going backward." << endl;
+  cout << getLogPrefix() << "Going backward." << endl;
   setStep(LOW, HIGH, LOW, HIGH);
 }
 
 void goLeft() {
-  cout << "Going left." << endl;
+  cout << getLogPrefix() << "Going left." << endl;
   setStep(LOW, HIGH, HIGH, LOW);
 }
 
 void goRight() {
-  cout << "Going right." << endl;
+  cout << getLogPrefix() << "Going right." << endl;
   setStep(HIGH, LOW, LOW, HIGH);
 }
 
@@ -94,27 +112,29 @@ int main(){
   
   pauze(3 * DELAY);
 
+  cout << "time: " << getCurrentTime() << endl;
+
   
   for(int i=0; i<STEPS; i+=4){
 
     cout << "Step " << i + 1 << endl;
     goForward();
-    delay(3 * DELAY);
+    //delay(3 * DELAY);
     pauze(2000);
 
     cout << "Step " << i + 2 << endl;
     goBackward();
-    delay(3 * DELAY);
+    //delay(3 * DELAY);
     pauze(2000);
 
     cout << "Step " << i + 3 << endl;
     goLeft();
-    delay(3 * DELAY);
+    //delay(3 * DELAY);
     pauze(2000);
 
     cout << "Step " << i + 4 << endl;
     goRight();
-    delay(3 * DELAY);
+    //delay(3 * DELAY);
     pauze(2000);
   }
 
